388b42ad36a66e5bfca9755653690f8cec6196dc
mdali_1 <32926705+forpix@users.noreply.github.com>
2021-01-20 13:14:42 +0530

    gcts configure

--- gcts.sh
+++ gcts.sh
@@ -2,37 +2,42 @@
 
 pwd
 
-echo ' <===> Lets Create the gcts folder  <===> '
+echo ' <==================> Lets Create the gcts folder  <======================> '
 
-cd /usr/sap/QE1/D00/test
+cd /usr/sap/QE1/D00/
 
-mkdir abc
+mkdir gcts
 
-chmod 777 abc
+chmod 777 gcts
 
 ls -a
 
-echo ' <===> Java Machine installation <===> '
+echo ' <===================> Java Machine installation <==========================> '
 
-cd /usr/sap/QE1/D00/test/
+cd /usr/sap/QE1/D00/
 
-mkdir def
+mkdir sapmachine
 
-chmod  777 def/
+chmod  777 sapmachine/
 
-cd /usr/sap/QE1/D00/test/def
+cd /usr/sap/QE1/D00/sapmachine
 
 pwd
 
-echo '======= ======== ========'
+bash
+
+zypper -n install ca-certificates curl
+
+curl -L -O https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.4/sapmachine-jdk-11.0.4-1.x86_64.rpm
+
+zypper -n --no-gpg-checks install ./sapmachine-jdk-11.0.4-1.x86_64.rpm
+
+echo '=============== +++++++++ =========== +++++++++ =============='
 
 java -version
 
-echo 'script is successfull executed '
-
-cd /usr/sap/QE1/D00/test/
-
 ls -a
 
+echo 'script is successfull executed '
 
 
