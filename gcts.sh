46893ddf2ee6c2f131e9d0a74c628ff02f627f78
mdali_1 <32926705+forpix@users.noreply.github.com>
2021-01-20 10:37:09 +0530

    created the gcts makeup script

---
+++ gcts.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+pwd
+
+echo '========= Let's Create the gcts folder =========='
+
+cd /usr/sap/QE1/D00/
+
+mkdir gcts
+
+chmod 777 gcts
+
+echo ' <===> Java Machine installation <===> '
+
+cd /usr/sap/QE1/D00
+
+mkdir sapmachine
+
+chmod  777 sapmachine/
+
+cd /usr/sap/QE1/D00/sapmachine
+
+pwd; ll 
+
+
+
